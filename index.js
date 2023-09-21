886fca53a60dcc5e2c834d3e0689ac4cf8b9b0e0
PRINCEGC0524 <142773112+PRINCEGC0524@users.noreply.github.com>
2023-09-21 21:31:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ export default function Home() {
   async function onSubmit(event) {
     event.preventDefault();
     try {
-      const response = await fetch("/api/generate", {
+      const response = await fetch("/https://us-central1-whisper-fine-tuning-dev.cloudfunctions.net/function-1", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
